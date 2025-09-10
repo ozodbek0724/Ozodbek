edfa12ca36d520f93f9ac84b7fbfd4ba15e12ee8
o.musurmonov <o.musurmonov@newuu.uz>
2025-09-10 13:38:43 +0500

    week 3 lab 2 problem from 11 to 22

--- main.cpp
+++ main.cpp
@@ -3,159 +3,159 @@
 
 using namespace std;
 
-/*//Problem 11
-
-int main() {
-   double a = 1.13e3;
-    double b = 411e-4;
-    cout << a << endl;
-    cout << b << endl;
-    return 0;
-}
-
-//Problem 12
-
-int main() {
-    int students = 241;
-    int groups = 11;
-    int firstGroup = students / groups;
-    int lastGroup = students % groups;
-    cout << "In the first Group: " << firstGroup << endl;
-    cout << "In the last Group: " << lastGroup << endl;
-    return 0;
-}
-
-//Problem 13
-
-int main() {
-
-    double s;
-    cin >> s;
-    double Area = ((3 * 1.73/2) * s * s);
-    cout << Area << endl;
-    return 0;
-}
-
-// Problem 14
-
-int main() {
-    int num = 31;
-    int a = num++;
-    int b = a * 3;
-    int c = 14- a;
-    int d = a / 4;
-    int e = a--;
-    int f = e % 9;
-    cout << num << endl;
-    cout << b << endl;
-    cout << c << endl;
-    cout << d << endl;
-    cout << a << endl;
-    cout << f << endl;
-    cout << a << endl;
-    return 0;
-}
-
-//Problem 15
-
-int main() {
-    char a = 'A';
-    a = a + 7;
-    cout << a;
-   a = a - 3;
-    cout << a;
-    a = a + 7;
-    cout << a;
-    cout << a;
-    a = a + 3;
-    cout << a;
-
-}
-
-//Problem 16
-
-int main() {
-    float x = 17;
-    float y = 1;
-    float d = ++y;
-    float t = ++y;
-    float z = (x + ++x) / (t * d);
-    cout << z << endl;
-
-}
-
-//Problem 17
-
+// //Problem 11
+//
 // int main() {
-//     int a , b;
-//     cin>>a>>b;
-//     cout<<(a==b)<< endl;
+//    double a = 1.13e3;
+//     double b = 411e-4;
+//     cout << a << endl;
+//     cout << b << endl;
 //     return 0;
 // }
-
-//Problem 18
-
-int main() {
-    int a, b;
-
-    cout << "Enter value for a: ";
-    cin >> a;
-    cout << "Enter value for b: ";
-    cin >> b;
-
-    bool result = (a < 140) && (a < b);
-
-    cout << (result ? 1 : 0) << endl;
-
-    return 0;
-
-}
-
-//Problem 19
-
-int main() {
-    int a, b, c;
-    cin >> a >> b >> c;
-    int result= (a>b) && (a>c);
-    cout << result << endl;
-    return 0;
-}
-
-//Problem 20
-
-int main() {
-    int num;
-    bool found = false;
-
-    for (int i = 0; i < 5; i++) {
-        cin >> num;
-        found = found || (num >= 0);
-    }
-
-    cout << found << endl;
-
-    return 0;
-}
-
-//Problem 21
-
+//
+// //Problem 12
+//
+// int main() {
+//     int students = 241;
+//     int groups = 11;
+//     int firstGroup = students / groups;
+//     int lastGroup = students % groups;
+//     cout << "In the first Group: " << firstGroup << endl;
+//     cout << "In the last Group: " << lastGroup << endl;
+//     return 0;
+// }
+//
+// //Problem 13
+//
+// int main() {
+//
+//     double s;
+//     cin >> s;
+//     double Area = ((3 * 1.73/2) * s * s);
+//     cout << Area << endl;
+//     return 0;
+// }
+//
+// // Problem 14
+//
+// int main() {
+//     int num = 31;
+//     int a = num++;
+//     int b = a * 3;
+//     int c = 14- a;
+//     int d = a / 4;
+//     int e = a--;
+//     int f = e % 9;
+//     cout << num << endl;
+//     cout << b << endl;
+//     cout << c << endl;
+//     cout << d << endl;
+//     cout << a << endl;
+//     cout << f << endl;
+//     cout << a << endl;
+//     return 0;
+// }
+//
+// //Problem 15
+//
+// int main() {
+//     char a = 'A';
+//     a = a + 7;
+//     cout << a;
+//    a = a - 3;
+//     cout << a;
+//     a = a + 7;
+//     cout << a;
+//     cout << a;
+//     a = a + 3;
+//     cout << a;
+//
+// }
+//
+// //Problem 16
+//
+// int main() {
+//     float x = 17;
+//     float y = 1;
+//     float d = ++y;
+//     float t = ++y;
+//     float z = (x + ++x) / (t * d);
+//     cout << z << endl;
+//
+// }
+//
+// //Problem 17
+//
+// // int main() {
+// //     int a , b;
+// //     cin>>a>>b;
+// //     cout<<(a==b)<< endl;
+// //     return 0;
+// // }
+//
+// //Problem 18
+//
 // int main() {
 //     int a, b;
-//     cin >> a >> b;
-//     cout << ((a + b > 30) && (a * b > 30));
-//     cout << ((a + b > 30) || (a * (b - 10) > 30));
-//     cout << ((a > 30) || (b > 30));
+//
+//     cout << "Enter value for a: ";
+//     cin >> a;
+//     cout << "Enter value for b: ";
+//     cin >> b;
+//
+//     bool result = (a < 140) && (a < b);
+//
+//     cout << (result ? 1 : 0) << endl;
+//
+//     return 0;
+//
+// }
+//
+// //Problem 19
+//
+// int main() {
+//     int a, b, c;
+//     cin >> a >> b >> c;
+//     int result= (a>b) && (a>c);
+//     cout << result << endl;
+//     return 0;
+// }
+//
+// //Problem 20
+//
+// int main() {
+//     int num;
+//     bool found = false;
+//
+//     for (int i = 0; i < 5; i++) {
+//         cin >> num;
+//         found = found || (num >= 0);
+//     }
+//
+//     cout << found << endl;
 //
 //     return 0;
 // }
-
-//Problem 22
-
-int main() {
-    double x, y;
-    cin >> x >> y;
-    double mikeHeightCm = y * 30.48;
-    cout << (mikeHeightCm > x ? 1 : 0) << endl;
-
-    return 0;
-}*/
+//
+// //Problem 21
+//
+// // int main() {
+// //     int a, b;
+// //     cin >> a >> b;
+// //     cout << ((a + b > 30) && (a * b > 30));
+// //     cout << ((a + b > 30) || (a * (b - 10) > 30));
+// //     cout << ((a > 30) || (b > 30));
+// //
+// //     return 0;
+// // }
+//
+// //Problem 22
+//
+// int main() {
+//     double x, y;
+//     cin >> x >> y;
+//     double mikeHeightCm = y * 30.48;
+//     cout << (mikeHeightCm > x ? 1 : 0) << endl;
+//
+//     return 0;
+}
